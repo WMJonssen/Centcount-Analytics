aac740c53e671e5c5a9f06c104560d6d8ba0787a
WM Jonssen <wm.jonssen@gmail.com>
2018-03-16 08:44:52 +0800

    Add files via upload

--- site/sites_all_sites.php
+++ site/sites_all_sites.php
@@ -1,10 +1,10 @@
 <?php
 /*!
 * ATTENTION: THIS FREE LICENSE IS ONLY FOR PERSONAL NON-COMMERCIAL USER. FOR COMMERCIAL PURPOSES, PLEASE PURCHASE A COMMERCIAL LICENSE! * 
-* module: Centcount Analytics Free Manage Sites PHP Code *
+* module: Centcount Analytics Free All Sites PHP Code *
 * version: 1.00 Free *
 * author: WM Jonssen *
-* date: 03/12/2018 *
+* date: 03/16/2018 *
 * copyright 2015-2018 WM Jonssen <wm.jonssen@gmail.com> - All rights reserved.*
 * license: Dual licensed under the Free License and Commercial License. *
 * https://www.centcount.com *
@@ -173,7 +173,7 @@ td{padding-left:5px; padding-right:5px;}
 						"<p class=\"plast\">" + SITES[row]["RV1"] + "</p>" +
 					"</td>" +
 					(w > 960 ? "<td class=\"tdmid\" ><a class=\"taba\" href=\"" + SITES[row]["Url"] + "&menu=Dashboard&action=Dashboard\">" + LAN["View"] + "</a></td>" +
-					"<td class=\"tdmidnoborder\" ><a class=\"taba\" href=\"" + SITES[row]["Url"] + "&menu=Sites&action=Get Code\">" + LAN["JS Code"] + "</a></td>" +
+					"<td class=\"tdmidnoborder\" ><a class=\"taba\" href=\"" + SITES[row]["Url"] + "&menu=Sites&action=Get JS Code\">" + LAN["JS Code"] + "</a></td>" +
 					"<td class=\"tdmidnoborder\" ><a class=\"taba\" href=\"" + SITES[row]["Url"] + "&menu=Sites&action=Domains\">" + LAN["Set"] + "</a></td>" +
 					"<td class=\"tdmidnoborder\" ><a class=\"taba\" href=\"" + SITES[row]["Url"] + "&menu=Sites&action=Delete Site\">" + LAN["Delete"] + "</a></td>" : "") +
 					"</tr>";
