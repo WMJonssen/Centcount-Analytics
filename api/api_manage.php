809d3dcdf1981239a7bf681d8e0c73b531f28688
WM Jonssen <wm.jonssen@gmail.com>
2018-05-03 09:46:16 +0800

    Update api_manage.php

--- api/api_manage.php
+++ api/api_manage.php
@@ -4,7 +4,7 @@
 * module: Centcount Analyticsb Free Manage Site API PHP Code *
 * version: 1.00 Free *
 * author: WM Jonssen *
-* date: 03/12/2018 *
+* date: 05/03/2018 *
 * copyright 2015-2018 WM Jonssen <wm.jonssen@gmail.com> - All rights reserved.*
 * license: Dual licensed under the Free License and Commercial License. *
 * https://www.centcount.com *
@@ -213,7 +213,7 @@ function check_value($value, $type) {
 function check_domain_ip($x) {
 		$tmp = parse_url($x, PHP_URL_HOST);
 		if (!$tmp) $tmp = $x;
-		if (preg_match("/^[0-9a-zA-Z]+[0-9a-zA-Z\.-]*\.[a-zA-Z]{2,4}$/", $tmp)) {
+		if (preg_match("/^[0-9a-zA-Z]+[0-9a-zA-Z\.-]*\.[a-zA-Z]{2,6}$/", $tmp)) {
 			return strtolower($tmp);
 		} else {
 			if ((bool)filter_var($tmp, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
